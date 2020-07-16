ef5d59bd12c5fca6df7f2664a2fe8ea295f7a0d5
lrwlfyang <lrwlfyang@tencent.com>
2020-07-16 09:38:41 +0800

    feat:搭建vue3 cli框架

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div>{{counter}}</div>
+    <button @click="addCount">+1</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
+import {
+  ref,
+} from 'vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  // components: {
+  //   HelloWorld,
+  // },
+  setup() {
+    const counter = ref(0);
+
+    const addCount = function () {
+      counter.value += 1;
+    };
+    return {
+      counter,
+      addCount,
+    };
   },
+
 };
 </script>
